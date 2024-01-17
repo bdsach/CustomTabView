2fd49ba476f8719eb7bb1de0a849d6f480a1f7de
Bandit Silachai <mbandit001@yahoo.co.th>
2024-01-17 09:03:15 +0700

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Bandit Silachai on 17/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
